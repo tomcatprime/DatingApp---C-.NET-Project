47d9cd3b032baaf903aa9007d6ee440810d9a39f
tomcatprime <admin@up2cloud.one>
2025-07-29 15:23:43 +0300

    Added DTO and changed to Account Controller

--- API/Controllers/AccountController.cs
+++ API/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using API.Data;
+using API.DTOs;
 using API.Entities;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
 using System.Security.Cryptography; // For hashing passwords
 
 namespace API.Controllers
@@ -9,14 +11,21 @@ namespace API.Controllers
     {
 
         [HttpPost("register")] // Endpoint to register a new user, api/account/register
-        public async Task<ActionResult<AppUser>> Register(string email, string displayName, string password)
+        public async Task<ActionResult<AppUser>> Register(RegisterDto registerDto) // RegisterDto contains the required data for registration
+
         {
+
+            if(await EmailExists(registerDto.Email)) return BadRequest("Email is already in use"); // Check if the email already exists, return BadRequest if it does
+            //we need to use await to ensure that the asynchronous operation completes before proceeding, this is important to avoid blocking the thread and to ensure that the application remains responsive
+
+
+
             using var hmac = new HMACSHA512(); // Create a new instance of HMACSHA512 for hashing passwords, word "using" ensures proper disposal of the object, freeing resources after use
             var user = new AppUser
             {
-                DisplayName = displayName,
-                Email = email,
-                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(password)), // Hash the password using HMACSHA512
+                DisplayName = registerDto.DisplayName,
+                Email = registerDto.Email,
+                PasswordHash = hmac.ComputeHash(System.Text.Encoding.UTF8.GetBytes(registerDto.Password)), // Hash the password using HMACSHA512
                 PasswordSalt = hmac.Key // Store the key used for hashing as the password salt
             };
 
@@ -26,5 +35,10 @@ namespace API.Controllers
             return Ok(user); // Return the created user with a 200 OK status
         }
 
+        private async Task<bool> EmailExists(string email) // Check if the email already exists in the database
+        {
+            return await context.Users.AnyAsync(x => x.Email.ToLower() == email.ToLower()); // Asynchronously check if any user has the specified email
+
+        }
     }
 }
---
+++ API/DTOs/RegisterDto.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations; // For data annotations used in model validation
+
+namespace API.DTOs
+{
+    public class RegisterDto
+    {
+        [Required] // Indicates that this property is required, meaning it must be provided when creating a new user, this is data annotation that enforces validation rules
+        public string DisplayName { get; set; } = ""; // Default value is an empty string, required for user registration
+
+        [Required]
+        [EmailAddress]
+
+        public string Email { get; set; } = ""; // Default value is an empty string, required for user registration and must be a valid email format
+
+        [Required]
+        [MinLength(4)]
+        public string Password { get; set; }
+    }
+}
